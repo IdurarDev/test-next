9cf72a7dd3c6ac69d485e641fbd2e9db34ee28d4
Hamid Berkaine <hamiddev78@gmail.com>
2023-01-09 10:38:54 +0100

    update function

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,12 +1,13 @@
 import Layout from '../../components/Layout';
 import { getAllPostIds, getPostData } from '../../lib/posts';
 
-export async function getStaticPaths() {
-  const paths = getAllPostIds();
-  return {
-    paths,
-    fallback: false,
-  };
+export async function getStaticProps({ params }) {
+    const postData = getPostData(params.id);
+    return {
+      props: {
+        postData,
+      },
+    };
 }
 
 export default function Post() {
