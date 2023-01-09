d00c3dc3984bff3825ea7240e87a98962eb8de99
Hamid Berkaine <hamiddev78@gmail.com>
2023-01-09 10:22:15 +0100

    change name in home page

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = 'Hamid Idurar';
+const name = 'Idurar & Co.';
 export const siteTitle = 'Next.js Sample Website';
 
 export default function Layout({ children, home }) {
